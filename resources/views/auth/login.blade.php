1fb577a32ebfcb23202af8f52912c5010b8ffeb9
Makhikhi Rasebolu <makhikhirasebolu@gmail.com>
2026-04-28 15:23:59 +0100

    fix https issue

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
                     </div>
                 @endif
 
-                <form method="POST" action="{{ route('login.submit') }}">
+                <form method="POST" action="{{ url('/login') }}">
                     @csrf
 
                     <div class="field">
